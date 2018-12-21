9d7cd41ee8daff8fdaac2d3c9a5441f6dbbb3848
Donald Hohenstein <dghohens@gmail.com>
2018-12-20 20:04:03 -0500

    Day 50 of 100 days of code, solar system model done!

--- PlanetSystem.py
+++ PlanetSystem.py
@@ -23,56 +23,27 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_xsize, screen_ysize))
 
 mercury_radius = 80
-mercury_xpos = 80
-mercury_ypos = 1
-mercury_ypositive = True
+mercury_degree = 0
+mercury_degree_change = 0.81818
 
 venus_radius = 170
-venus_xpos = 170
-venus_ypos = 1
-venus_ypositive = True
+venus_degree = 0
+venus_degree_change = 0.32
 
 earth_radius = 240
-earth_xpos = 240
-earth_ypos = 1
-earth_ypositive = True
+earth_degree = 0
+earth_degree_change = 0.19672
 
 mars_radius = 350
-mars_xpos = 350
-mars_ypos = 0
 mars_degree = 0
+mars_degree_change = 0.1048
 
 moon_radius = 25
-moon_xpos = 25
-moon_ypos = 1
-moon_ypositive = True
+moon_degree = 0
+moon_degree_change = 2.66667
 
-
-def planetmove(rad, xpos, ypos, ypositive):
-    nearend = rad - abs(xpos)
-
-    if nearend <= 4 and ypositive:
-        xpos -= 0.4
-    elif nearend <= 4 and ypositive is False:
-        xpos += 0.4
-    elif ypositive:
-        xpos -= 1
-    else:
-        xpos += 1
-
-    if xpos >= rad:
-        xpos = rad
-    elif xpos <= -rad:
-        xpos = -rad
-
-    ypos = ((rad ** 2) - (xpos ** 2)) ** (1 / 2)
-
-    if ypositive is False:
-        ypos = -1 * ypos
-    return xpos, ypos
-
-def degreemove(angle, radius):
-    angle += 1
+def degreemove(angle, radius, angle_change):
+    angle += angle_change
     xpos = math.cos(math.radians(angle)) * radius
     ypos = math.sin(math.radians(angle)) * radius
     return angle, xpos, ypos
@@ -86,56 +57,30 @@ while True:
 
     screen.fill(black)
 
-    if mercury_ypos == 0:
-        if mercury_ypositive:
-            mercury_ypositive = False
-        elif mercury_ypositive is False:
-            mercury_ypositive = True
-    mercury_pos = planetmove(mercury_radius, mercury_xpos, mercury_ypos, mercury_ypositive)
-    mercury_xpos = mercury_pos[0]
-    mercury_ypos = mercury_pos[1]
+    mercury_pos = degreemove(mercury_degree, mercury_radius, mercury_degree_change)
+    mercury_degree = mercury_pos[0]
+    mercury_xpos = mercury_pos[1]
+    mercury_ypos = mercury_pos[2]
 
-    if venus_ypos == 0:
-        if venus_ypositive:
-            venus_ypositive = False
-        elif venus_ypositive is False:
-            venus_ypositive = True
-    venus_pos = planetmove(venus_radius, venus_xpos, venus_ypos, venus_ypositive)
-    venus_xpos = venus_pos[0]
-    venus_ypos = venus_pos[1]
+    venus_pos = degreemove(venus_degree, venus_radius, venus_degree_change)
+    venus_degree = venus_pos[0]
+    venus_xpos = venus_pos[1]
+    venus_ypos = venus_pos[2]
 
-    if earth_ypos == 0:
-        if earth_ypositive:
-            earth_ypositive = False
-        elif earth_ypositive is False:
-            earth_ypositive = True
-    earth_pos = planetmove(earth_radius, earth_xpos, earth_ypos, earth_ypositive)
-    earth_xpos = earth_pos[0]
-    earth_ypos = earth_pos[1]
+    earth_pos = degreemove(earth_degree, earth_radius, earth_degree_change)
+    earth_degree = earth_pos[0]
+    earth_xpos = earth_pos[1]
+    earth_ypos = earth_pos[2]
 
-    '''if mars_ypos == 0:
-        if mars_ypositive:
-            mars_ypositive = False
-        elif mars_ypositive is False:
-            mars_ypositive = True
-    mars_pos = planetmove(mars_radius, mars_xpos, mars_ypos, mars_ypositive)
-    mars_xpos = mars_pos[0]
-    mars_ypos = mars_pos[1]
-    '''
-
-    mars_pos = degreemove(mars_degree, mars_radius)
+    mars_pos = degreemove(mars_degree, mars_radius, mars_degree_change)
     mars_degree = mars_pos[0]
     mars_xpos = mars_pos[1]
     mars_ypos = mars_pos[2]
 
-    if int(moon_ypos) == 0:
-        if moon_ypositive:
-            moon_ypositive = False
-        elif moon_ypositive is False:
-            moon_ypositive = True
-    moon_pos = planetmove(moon_radius, moon_xpos, moon_ypos, moon_ypositive)
-    moon_xpos = moon_pos[0]
-    moon_ypos = moon_pos[1]
+    moon_pos = degreemove(moon_degree, moon_radius, moon_degree_change)
+    moon_degree = moon_pos[0]
+    moon_xpos = moon_pos[1]
+    moon_ypos = moon_pos[2]
 
     # Sun
     pygame.draw.circle(screen, yellow, center, 40)
